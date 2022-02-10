9005c48353ff0bfb4579f90b0e58d6b947c1de57
dieg0code <diegoobando20@gmail.com>
2022-02-10 18:13:36 -0300

    swagger docs & prisma schema

---
+++ src/main.ts
@@ -0,0 +1,20 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const config  = new DocumentBuilder()
+  .setTitle('Cargo five test')
+  .setDescription('Test')
+  .setVersion('1.0')
+  .addTag('cargo five')
+  .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document, { customSiteTitle: 'Cargo five test' });
+
+  await app.listen(3000);
+}
+bootstrap();
